808c5a58d1d4f76001e9e750d3947584f8d10ce6
Yuuki the Handyfox (João Costa) <joaoinstaller@gmail.com>
2020-08-23 20:25:09 -0400

    The kernel is finally here!

---
+++ kernel/kernel.c
@@ -0,0 +1,16 @@
+/* Naka-chan - The OS kernel!
+   --------------------------------
+   Finally, the heart of our OS!
+   I present you... Naka-chan!
+   Code: Yuuki Sanada
+*/
+// Here we'll use a dummy function, just to
+// avoid loading kernel.c:0x00
+void dummy_test_entrypoint() {
+}
+// And here's our main function!
+// Don't mind, it's still barebones by now.
+void main() {
+    char* video_memory = (char*) 0xb8000;
+    *video_memory = '1';
+}

a4e226d8467605406770f4f4f55a99791e6f6739
Yuuki the Handyfox (João Costa) <joaoinstaller@gmail.com>
2020-09-08 19:50:40 -0400

    Bootloader rewritten

--- boot/disksect.asm
+++
@@ -1,8 +0,0 @@
-; Yomite-chan - Disk reader
-; --------------------------------
-; A boot sector reader. It helps
-; Dokusha-chan, so to say.
-; Let's pretend it's her twin
-; sister.
-; Code: Yuuki Sanada
-; --------------------------------
--- boot/kernel_entry.asm
+++
@@ -1,8 +0,0 @@
-global _start
-[bits 32]
-[extern kernel_main]
-_start:
-    ; Here we'll call our kernel function.
-    call kernel_main
-    ; And get into an infinite loop.
-    jmp $

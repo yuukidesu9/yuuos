16110be57c4afd3ed04d6b40c119fb27dc4907c7
Yuuki the Handyfox (João Costa) <joaoinstaller@gmail.com>
2020-09-16 20:49:16 -0400

    Initial video scroll implementation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,6 +6,8 @@
 */
 // Let's call our textmode routines...
 #include "../drivers/screen.h"
+// ..and our utilities.
+#include "util.h"
 // And here's our main function!
 // Don't mind, it's a bit barebones by now.
 void kernel_main() {
@@ -14,4 +16,6 @@ void kernel_main() {
     kprint("\nAw yiss, babe! It works!\n");
     kprint_at("h", 45, 24);
     kprint_at("Welcome to yuuOS", 30, 11);
+    kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60, 24);
+    kprint("And with this text, the kernel will scroll again, and row 1 will disappear too!");
 }

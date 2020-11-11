97bd4104902604f53e34413bf14d8d1e83265871
Yuuki the Handyfox (João Costa) <joaoinstaller@gmail.com>
2020-11-11 14:55:05 -0400

    Added Caps Lock support to shell.

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -8,28 +8,29 @@
 
 #define BACKSPACE 0x0E
 #define ENTER 0x1C
+#define CAPSLOCK 0x3A
 
 static char key_buffer[256];
 
-#define SC_MAX 57
+#define SC_MAX 58
 const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
     "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
         "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
         "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
         "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
+        "/", "RShift", "Keypad *", "LAlt", "Spacebar", "CapsLock"};
 const char sc_ascii[] = { '\0', '\0', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '\0', '\0', 'q', 'w', 'e', 'r', 't', 'y', 
         'u', 'i', 'o', 'p', '[', ']', '0', '\0', 'a', 's', 'd', 'f', 'g', 
         'h', 'j', 'k', 'l', ';', '\'', '`', '\0', '\\', 'z', 'x', 'c', 'v', 
-        'b', 'n', 'm', ',', '.', '/', '\0', '\0', '\0', ' '};
+        'b', 'n', 'm', ',', '.', '/', '\0', '\0', '\0', ' ', '\0'};
 const char sc_ascii_caps[] = { '\0', '\0', '!', '@', '#', '$', '%', '^',     
     '&', '*', '(', ')', '_', '+', '\0', '\0', 'Q', 'W', 'E', 'R', 'T', 'Y', 
         'U', 'I', 'O', 'P', '{', '}', '\0', '\0', 'A', 'S', 'D', 'F', 'G', 
         'H', 'J', 'K', 'L', ':', '\"', '~', '\0', '|', 'Z', 'X', 'C', 'V', 
-        'B', 'N', 'M', '<', '>', '?', '\0', '\0', '\0', ' '};
+        'B', 'N', 'M', '<', '>', '?', '\0', '\0', '\0', ' ', '\0'};
 
-int isCaps;
+static int isCaps;
 
 static void keyboard_callback(registers_t regs) {
     /* The PIC leaves us the scancode in port 0x60 */
@@ -51,6 +52,14 @@ static void keyboard_callback(registers_t regs) {
     if (scancode == 0xAA){
         isCaps = 0;
     }
+    if (scancode == CAPSLOCK){
+        if(isCaps){
+            isCaps = 0;
+        }
+        else{
+            isCaps = 1;
+        }
+    }
     if (scancode > SC_MAX) return;
     if (scancode == BACKSPACE) {
         backspace(key_buffer);
@@ -60,7 +69,7 @@ static void keyboard_callback(registers_t regs) {
         user_input(key_buffer);
         key_buffer[0] = '\0';
     } else {
-        if(isCaps == 1){
+        if(isCaps){
             letter = sc_ascii_caps[(int)scancode];
         }
         else{
